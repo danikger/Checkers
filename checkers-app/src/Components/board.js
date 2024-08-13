9630ba108fc639b04b13d9e1eed66cfb7825a03c
Daniil Gerachshenko <danikger@gmail.com>
2024-08-13 13:14:30 -0500

    Adds a border to the board and crowns to the pieces

--- checkers-app/src/Components/board.js
+++ checkers-app/src/Components/board.js
@@ -1,3 +1,5 @@
+import { FaCrown } from "react-icons/fa";
+
 export default function Board({ board }) {
 
   // PIECE VALUES
@@ -9,18 +11,20 @@ export default function Board({ board }) {
 
   return (
     <>
-      <div className="grid grid-cols-8 grid-rows-8 mt-32 aspect-square rounded-lg overflow-hidden">
+      <div className="grid grid-cols-8 grid-rows-8 mt-32 aspect-square rounded-lg overflow-hidden border-8 border-gray-950">
         {board.map((row, rowIndex) => (
           <>
             {row.map((square, colIndex) => (
               <div
                 key={colIndex}
-                className={`flex justify-center items-center cursor-pointer aspect-square ${(rowIndex + colIndex) % 2 === 0 ? 'bg-gray-700' : 'bg-gray-900'}`}
+                className={`flex justify-center items-center aspect-square ${(rowIndex + colIndex) % 2 === 0 ? 'bg-gray-700' : 'bg-gray-900'} ${square !== 0 ? 'cursor-pointer' : ''}`}
               >
                 {square !== 0 && (
                   <div
-                    className={`w-full m-3 rounded-full aspect-square  ${square === 1 || square === 3 ? 'bg-red-500 border-8 border-red-700' : 'bg-gray-100 border-8 border-gray-300'} ${square === 3 || square === 4 ? 'border-yellow-600' : ''}`}
-                  />
+                    className={`w-full flex m-3 rounded-full aspect-square border-4 sm:border-8 ${square === 1 || square === 3 ? 'bg-red-500 border-red-700' : 'bg-gray-100 border-gray-300'} ${square === 3 || square === 4 ? 'border-yellow-600' : ''}`}
+                  >
+                    <FaCrown className={`m-auto h-7 w-7 hidden ${square === 3 || square === 4 ? 'block text-red-800' : 'block text-gray-400'}`}/>
+                  </div>
                 )}
               </div>
             ))}
