9ac1dc3cbf1d350bc3458d797ef0ee28cbc1b321
Daniil Gerachshenko <danikger@gmail.com>
2024-08-16 22:42:08 -0500

    Adds a comment to generateGameId util function

--- checkers-app/src/utils/generateGameId.js
+++ checkers-app/src/utils/generateGameId.js
@@ -1,3 +1,7 @@
+/**
+ * Generates a random string of 9 characters.
+ * @returns {string} - A random string of 9 characters
+ */
 export default function generateGameId() {
     return Math.random().toString(36).substr(2, 9);
 };
