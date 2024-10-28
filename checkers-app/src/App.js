33dd60a7a28b2540b082470bd273801da14b8e35
Daniil Gerachshenko <danikger@gmail.com>
2024-10-28 15:45:47 -0500

    Updates App to support matchmaking and improve code readability

--- checkers-app/src/App.js
+++ checkers-app/src/App.js
@@ -26,6 +26,8 @@ function App() {
   const [gameStarted, setGameStarted] = useState(false);
   const [currentPlayer, setCurrentPlayer] = useState(2); // Keeps track of whose turn it is. 1: Red's turn, 2: White's turn
   const [playerRole, setPlayerRole] = useState(2); // Tracks the player's color. 1: Red, 2: White
+  const [itemType, setItemType] = useState(''); // Tracks the type of item being sent to the API (game or lobby)
+  const [username, setUsername] = useState(''); // Tracks the player's username
 
   const [redPieces, setRedPieces] = useState(12); // Tracks red pieces left on the board
   const [whitePieces, setWhitePieces] = useState(12); // Tracks white pieces left on the board
@@ -58,7 +60,7 @@ function App() {
       sessionStorage.setItem('gameId', JSON.stringify(urlGameId));
     }
 
-    // Checks if user is a guest
+    // Checks if user is a guest and sets the appriopriate player role and board state
     if (!isHost) {
       const urlParams = new URLSearchParams(window.location.search);
       let urlGameId = urlParams.get('gameId');
@@ -95,15 +97,17 @@ function App() {
   const socketURL = 'wss://k81ymo1nek.execute-api.us-east-1.amazonaws.com/production/';
 
   const { sendMessage, lastMessage, readyState, getWebSocket } = useWebSocket(socketURL, {
-    queryParams: { gameId: gameId },
+    queryParams: { gameId: gameId, itemType: itemType, username: username },
     // shouldReconnect: () => false,
     onOpen: () => console.log('Connected'),
     onClose: () => {
       console.log('Disconnected');
-      setIsConnected(false);
-      setDisconnectType('player');
-      setDisconnectModal(true);
-      setGameStarted(false);
+      if (gameStarted) {
+        setIsConnected(false);
+        setDisconnectType('player');
+        setDisconnectModal(true);
+        setGameStarted(false);
+      }
     },
     onError: (error) => {
       console.log('WebSocket Error: ', error);
@@ -115,7 +119,10 @@ function App() {
   /**
    * Handles connecting to the API
    */
-  const connectWebsocket = () => {
+  const connectWebsocket = (newGameId, itemType, username) => {
+    setGameId(newGameId);
+    setItemType(itemType);
+    setUsername(username);
     setIsConnected(true);
   };
 
@@ -136,7 +143,7 @@ function App() {
    * Handles sending messages to the API
    */
   const sendMessageWebsocket = (type, lobbyId = gameId, message) => {
-    const messageObj = JSON.stringify({ action: "sendMessage", message: { message: message, gameId: lobbyId, type: type } });
+    const messageObj = JSON.stringify({ action: "sendMessage", data: { data: message, gameId: lobbyId, type: type } });
     console.log('Sending message:', messageObj);
     sendMessage(messageObj);
   };
@@ -147,36 +154,41 @@ function App() {
     if (lastMessage !== null) {
       let receivedMessage = JSON.parse(lastMessage.data);
       console.log(receivedMessage);
-      if (receivedMessage.type === 'start') {
-        console.log('Game started');
-        setGameStarted(true);
-        setOpenStartModal(false);
-      }
-      if (receivedMessage.type === 'disconnect') {
-        console.log('Game disconnected');
-        setDisconnectModal(true);
-        setDisconnectType('opponent');
-        setGameStarted(false);
-      }
-      if (receivedMessage.type === 'move') {
-        setBoard(flipGameBoard(receivedMessage.message.board)); // Update the board with the move from the opponent
-        playerRole === 1 ? setRedPieces((prevState) => prevState - receivedMessage.message.piecesCaptured) : setWhitePieces((prevState) => prevState - receivedMessage.message.piecesCaptured); // Set new number of pieces
-        setCurrentPlayer(currentPlayer === 1 ? 2 : 1); // Switch turns
-      }
-      if (receivedMessage.type === 'defeat') {
-        setEndCondition(`victory-${receivedMessage.message.defeatType}`); // Opponent either has a stalemate or conceded
-        setEndModal(true);
-        setGameStarted(false);
-      }
-      if (receivedMessage.type === 'rematch_request') {
-        if (rematchPending) {
-          // Both players agreed to a rematch, reset the game
-          resetGame();
-        } else {
-          // The other player requested a rematch, prompt the current player
-          setEndCondition('rematch_requested');
+
+      switch (receivedMessage.type) {
+        case 'start':
+          setGameStarted(true);
+          setOpenStartModal(false);
+          break;
+
+        case 'disconnect':
+          setDisconnectModal(true);
+          setDisconnectType('opponent');
+          setGameStarted(false);
+          break;
+
+        case 'move':
+          setBoard(flipGameBoard(receivedMessage.data.board)); // Update the board with the move from the opponent
+          playerRole === 1 ? setRedPieces((prevState) => prevState - receivedMessage.data.piecesCaptured) : setWhitePieces((prevState) => prevState - receivedMessage.data.piecesCaptured); // Set new number of pieces
+          setCurrentPlayer(currentPlayer === 1 ? 2 : 1); // Switch turns
+          break;
+
+        case 'defeat':
+          setEndCondition(`victory-${receivedMessage.data.defeatType}`); // Opponent either has a stalemate or conceded
           setEndModal(true);
-        }
+          setGameStarted(false);
+          break;
+
+        case 'rematch_request':
+          if (rematchPending) {
+            // Both players agreed to a rematch, reset the game
+            resetGame();
+          } else {
+            // The other player requested a rematch, prompt the current player
+            setEndCondition('rematch_requested');
+            setEndModal(true);
+          }
+          break;
       }
     }
   }, [lastMessage]);
@@ -226,7 +238,7 @@ function App() {
    * Resets the game to its initial state. Clears the board, resets the pieces, starts game, closes modal, and sets the current player to white.
    */
   function resetGame() {
-    setBoard(!isHost ? flipGameBoard(initialBoard) : initialBoard);
+    setBoard(isHost ? initialBoard : flipGameBoard(initialBoard)); // If player is the host, set the board to the initial state. If player is a guest, flip the board.
     setRedPieces(12);
     setWhitePieces(12);
     setGameStarted(true);
@@ -236,43 +248,42 @@ function App() {
     setEndCondition('');
   }
 
-  /**
-   * Handles the stalemate condition by displaying the end modal and sending a defeat message to the opponent.
-   */
-  function handleStalemate() {
-    if (redPieces !== 0 && whitePieces !== 0) {
-      setEndModal(true);
-      setEndCondition('loss-stalemate');
-      setGameStarted(false);
-      sendMessageWebsocket("defeat", undefined, { defeatType: 'stalemate' });
-    }
-  }
 
   /**
-   * Handles the concede condition by displaying the end modal and sending a defeat message to the opponent.
+   * Handles the game end condition by displaying the end modal and sending a defeat message to the opponent.
+   * 
+   * @param {string} defeatType The type of defeat that ended the game. 'stalemate', 'concede', or 'timeout'.
    */
-  function handleConcede() {
+  function handleGameEnd(defeatType) {
     setEndModal(true);
-    setEndCondition('loss-concede');
+    setEndCondition(`loss-${defeatType}`);
     setGameStarted(false);
-    sendMessageWebsocket("defeat", undefined, { defeatType: 'concede' });
+    sendMessageWebsocket("defeat", undefined, { defeatType: defeatType });
   }
 
 
+  // Handles the stalemate condition by the current player
+  function handleStalemate() {
+    if (redPieces !== 0 && whitePieces !== 0) handleGameEnd('stalemate');
+  }
+
+
+  // Handles the concede condition by the current player
+  function handleConcede() {
+    handleGameEnd('concede');
+  }
+
+
+  // Handles the timeout condition by the current player
   function handleTimeout() {
-    if (currentPlayer === playerRole) {
-      setEndModal(true);
-      setEndCondition('loss-timeout');
-      setGameStarted(false);
-      sendMessageWebsocket("defeat", undefined, { defeatType: 'timeout' });
-    }
-  };
+    if (currentPlayer === playerRole) handleGameEnd('timeout');
+  }
 
 
   return (
     <>
       <main className="bg-gray-900 min-h-screen absolute w-full">
-        <StartModal openStartModal={openStartModal} connectWebsocket={connectWebsocket} setGameId={setGameId} />
+        <StartModal openStartModal={openStartModal} connectWebsocket={connectWebsocket} disconnectWebsocket={disconnectWebsocket} sendMessageWebsocket={sendMessageWebsocket} lastMessage={lastMessage} />
         <DisconnectModal openDisconnectModal={openDisconnectModal} disconnectType={disconnectType} />
         <EndModal openEndModal={openEndModal} endCondition={endCondition} onRematch={handleRematch} rematchPending={rematchPending} />
         <div className="max-w-4xl mx-auto mt-16">
